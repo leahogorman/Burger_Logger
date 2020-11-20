3a2993fa9eafb7d8d31812ca9b755e05cfb32f3b
Leah O'Gorman <leah.p.ogorman@gmail.com>
2020-11-20 15:03:34 -0500

    added to files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 USE `burgers_db`;
 
-INSERT INTO burgers ( `burger_name`, `devoured` ) VALUES ("The Ulti-MEAT-um", "true");
+INSERT INTO burgers ( `burger_name` ) VALUES ("The Ulti-MEAT-um");
 INSERT INTO burgers ( `burger_name` ) VALUES ("The Pleased to MEAT You");
 INSERT INTO burgers ( `burger_name` ) VALUES ("The IncogMEATo");
-INSERT INTO burgers ( `burger_name`, `devoured` ) VALUES ("The MOO-na Lisa", "true");
+INSERT INTO burgers ( `burger_name` ) VALUES ("The MOO-na Lisa");
---
+++ public/assets/js/burger.js
@@ -0,0 +1,54 @@
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newDevoured");
+
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevoured);
+        location.reload();
+      }
+    );
+  });
+
+  $(".new-burger").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#name").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        location.reload();
+      }
+    );
+  });
+});
