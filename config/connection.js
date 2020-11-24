4c04176abd178775c7d6210be7947381541e353d
Leah O'Gorman <leah.p.ogorman@gmail.com>
2020-11-24 10:08:13 -0500

    debugging

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  console.log("connected to jawsDB hopefully!")
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password:  "password",
+    database: 'burgers_db'
+  });
+};
+
 
 connection.connect(function(err) {
   if (err) {
@@ -16,32 +23,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+
 module.exports = connection;
-
-
-// // Set up MySQL connection.
-// var mysql = require("mysql");
-// var connection;
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: process.env.DB_USER,
-//     password:  process.env.DB_PASSWORD,
-//     database: 'burgers_db'
-//   });
-// };
-
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-
-// module.exports = connection;
